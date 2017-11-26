195206c03cd9802897d3babc34a92f38f1ac12fc
Gil Gonçalves <lursty@gmail.com>
2017-11-26 20:25:21 +0000

    First try at this kata

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,33 +5,44 @@ class GildedRose:
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
+            is_backstage_pass = item.name == "Backstage passes to a TAFKAL80ETC concert"
+            is_aged_brie = item.name == "Aged Brie"
+            is_sulfuras = item.name == "Sulfuras, Hand of Ragnaros"
+            is_conjured = item.name == 'Conjured'
+            can_decrease_quality = item.quality > 0 and not is_sulfuras
+
+            if not is_aged_brie and not is_backstage_pass:
+                if can_decrease_quality:
+                    item.quality -= 1
+                if is_conjured:
+                    item.quality -= 1
+            elif is_backstage_pass:
+                item.quality += 1
+
+                if item.sell_in < 11:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+
+                if item.sell_in < 6:
+                    item.quality = item.quality + 1
+            else:
+                item.quality = item.quality + 1
+
+            if not is_sulfuras:
                 item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                if not is_aged_brie:
+                    if not is_backstage_pass and can_decrease_quality:
+                        item.quality -= 1
+                        if is_conjured:
+                            item.quality -= 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality += 1
+
+            if item.quality > 50:
+                item.quality = 50
 
 
 class Item:
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -3,14 +3,136 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
+class TestAgedBrie:
 
-    def test_foo(self):
-        items = [Item('foo', 0, 0)]
-        gilded_rose = GildedRose(items)
+    def test_aged_brie_with_high_quality_after_one_day(self):
+        item = Item('Aged Brie', 4, 50)
+        gilded_rose = GildedRose([item])
         gilded_rose.update_quality()
-        self.assertEquals('fixme', items[0].name)
+
+        assert item.sell_in == 3
+        assert item.quality == 50
+
+    def test_aged_brie_after_one_day(self):
+        item = Item('Aged Brie', 4, 10)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 3
+        assert item.quality == 11
+
+    def test_aged_brie_after_expiring(self):
+        item = Item('Aged Brie', 1, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+
+        assert item.sell_in == -1
+        assert item.quality == 13
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestSulfuras:
+
+    def test_sulfuras_after_one_day(self):
+        item = Item('Sulfuras, Hand of Ragnaros', 4, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 4
+        assert item.quality == 10
+
+
+class TestBackstagePasses:
+
+    def test_backstagepasses_high_quality_after_one_day(self):
+        item = Item('Backstage passes to a TAFKAL80ETC concert', 4, 50)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 3
+        assert item.quality == 50
+
+    def test_backstagepasses_after_one_day(self):
+        item = Item('Backstage passes to a TAFKAL80ETC concert', 4, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 3
+        assert item.quality == 13
+
+    def test_backstagepasses_after_expiring(self):
+        item = Item('Backstage passes to a TAFKAL80ETC concert', 1, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+
+        assert item.sell_in == -1
+        assert item.quality == 0
+
+    def test_backstagepasses_after_one_day_more_than_5_days_left(self):
+        item = Item('Backstage passes to a TAFKAL80ETC concert', 8, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 7
+        assert item.quality == 12
+
+
+class TestGenericItem:
+
+    def test_genericitem_after_one_day(self):
+        item = Item('Generic', 5, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 4
+        assert item.quality == 9
+
+    def test_genericitem_after_expiring(self):
+        item = Item('Generic', 1, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+
+        assert item.sell_in == -1
+        assert item.quality == 7
+
+    def test_genericitem_after_expiring_more(self):
+        item = Item('Generic', 1, 10)
+        gilded_rose = GildedRose([item])
+
+        for _ in range(10):
+            gilded_rose.update_quality()
+
+        assert item.sell_in == -9
+        assert item.quality == 0
+
+
+class TestConjured:
+
+    def test_conjured_after_one_day(self):
+        item = Item('Conjured', 10, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+
+        assert item.sell_in == 9
+        assert item.quality == 8
+
+    def test_conjured_after_expiring(self):
+        item = Item('Conjured', 1, 10)
+        gilded_rose = GildedRose([item])
+
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+
+        assert item.sell_in == -1
+        assert item.quality == 4
