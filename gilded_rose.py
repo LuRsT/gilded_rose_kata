0aa6631d24c64ccbfcf20b052886eab8e5d04a89
Gil Gonçalves <lursty@gmail.com>
2026-02-13 15:16:02 +0000

    [polymorphism] Fix test
    - move all conditions into item

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,33 +5,7 @@ class GildedRose:
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_quality()
 
 
 class Item:
@@ -42,3 +16,32 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update_quality(self):
+        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
+            if self.quality > 0:
+                if self.name != "Sulfuras, Hand of Ragnaros":
+                    self.quality = self.quality - 1
+        else:
+            if self.quality < 50:
+                self.quality = self.quality + 1
+                if self.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if self.sell_in < 11:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+                    if self.sell_in < 6:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+        if self.name != "Sulfuras, Hand of Ragnaros":
+            self.sell_in = self.sell_in - 1
+        if self.sell_in < 0:
+            if self.name != "Aged Brie":
+                if self.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if self.quality > 0:
+                        if self.name != "Sulfuras, Hand of Ragnaros":
+                            self.quality = self.quality - 1
+                else:
+                    self.quality = self.quality - self.quality
+            else:
+                if self.quality < 50:
+                    self.quality = self.quality + 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -3,14 +3,19 @@ from gilded_rose import Item, GildedRose
 
 class TestGildedRose:
 
-    def test_foo(self):
+    def test_new_item_foo(self):
         # Create item
         item = Item('foo', 0, 0)
+
         # Place it in gilded rose inn
         gilded_rose = GildedRose([item])
 
         # Make a day go by
         gilded_rose.update_quality()
 
-        # Check what happens
-        assert item.name == 'fixme'
+        # Item sell_in should decrease by one
+        assert item.sell_in == -1
+
+        # Item attributes should remain
+        assert item.name == "foo"
+        assert item.quality == 0
